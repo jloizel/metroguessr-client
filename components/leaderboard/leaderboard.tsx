8a77fabf22c7d4e9aa49c08d397b0d1525e279d9
Jack <jmloizel@gmail.com>
2024-05-02 23:01:30 +0100

    updates

--- components/leaderboard/leaderboard.tsx
+++ components/leaderboard/leaderboard.tsx
@@ -349,7 +349,7 @@ const Leaderboard: React.FC<LeaderboardProps> = ({ username, points, city, timeE
 
   const getMedalsGap = () => { // Default width for computer
     if (isMobile) {
-      return "4px";
+      return "8px";
     } else if (isTablet) {
       return "5px"; // Adjust width for tablet
     } else {
