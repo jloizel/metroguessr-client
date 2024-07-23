8b5f49d7cb59590d3d11878dc2ecd1383d8b5edb
Jack <jmloizel@gmail.com>
2024-07-23 22:38:10 +0100

    trying to add map style

--- components/map/map.tsx
+++ components/map/map.tsx
@@ -12,6 +12,7 @@ import NewYorkCityStyles from "./NYC.module.css"
 import MadridStyles from "./Madrid.module.css"
 import BarcelonaStyles from "./Madrid.module.css"
 import { tubeDataConfig } from '../../api/tubeDataConfig';
+import mapStyle from "../../public/style.json"
 
 interface Feature {
   type: string;
@@ -116,11 +117,21 @@ const Map: React.FC<MapProps> = ({selectedCity, gameStarted, randomStation, disa
 
   const map_api = process.env.MAP_API
 
+  const [mapStyle, setMapStyle] = useState("")
+
   useEffect(() => {
-    if (!map.current && map_api) {
+    async function initializeMap() {
+      const response = await fetch('/style.json'); // Fetch the JSON style file
+      setMapStyle(await response.json())
+    }
+    initializeMap();
+  })
+
+  useEffect(() => {
+    if (!map.current) {
       map.current = new maplibregl.Map({
         container: mapContainer.current!,
-        style: "https://api.maptiler.com/maps/36d47c22-ba26-40a6-8391-46de9e42e858/style.json?key=n8F7h6F3j80GK3qCDE3B",
+        style: mapStyle,
         center: mapCoords,
         minZoom: 11,
         maxZoom: 18,
